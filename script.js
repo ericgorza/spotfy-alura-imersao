fec0749396374ac168ad34b2aedfe98408eb03f3
Eric <ericgorza@gmail.com>
2024-01-25 13:40:48 -0800

    added some JS and CSS

---
+++ script.js
@@ -0,0 +1,34 @@
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById('result-artist')
+const resultPlaylist = document.getElementById('result-playlists')
+
+const displayResults = (result) => {
+    resultPlaylist.classList.add('hidden');
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtist.classList.remove('hidden');
+}
+
+function requestApi(searchTerm){
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
+}
+
+
+document.addEventListener('input', () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    if(searchTerm === ''){
+        resultPlaylist.classList.add('hidden')
+        resultArtist.classList.remove('hidden')
+        return;
+    }
+})
+
